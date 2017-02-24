7f9e201381554746fe0ea6e4730c678579006323
Steve Kondik <shade@chemlab.org>
2017-02-24 14:53:18 -0800

    uchroma: Clean up __init__

--- uchroma/__init__.py
+++ uchroma/__init__.py
@@ -1,11 +1,8 @@
+from .dbus_client import UChromaClient
 from .color import ColorScheme, ColorPair, ColorUtils
 from .device_manager import UChromaDeviceManager
-from .device import UChromaDevice
 from .frame import Frame
 from .hardware import Hardware
-from .headset import EffectBits
 from .layer import Layer
-from .led import LED
-from .mouse import PollingRate
-from .report import Status
+from .renderer import Renderer
 from .version import __version__
