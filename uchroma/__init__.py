9897625d5ed896e33ca462566da30dba211820fb
Steve Kondik <shade@chemlab.org>
2017-03-13 22:11:42 -0700

    uchroma: Remove unnecessary stuff from top-level init

--- uchroma/__init__.py
+++ uchroma/__init__.py
@@ -1,5 +1,1 @@
-from .color import ColorScheme, ColorPair, ColorUtils
-from .input_queue import KeyInputEvent, InputQueue
-from .layer import Layer
-from .renderer import Renderer
 from .version import __version__
